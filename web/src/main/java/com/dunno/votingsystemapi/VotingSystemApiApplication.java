4c2256a43bdcf98b7dad88a69a76d061133e9790
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-25 15:35:13 -0500

    Initial Configuration

---
+++ web/src/main/java/com/dunno/votingsystemapi/VotingSystemApiApplication.java
@@ -0,0 +1,12 @@
+package com.dunno.votingsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemApiApplication.class, args);
+    }
+}
