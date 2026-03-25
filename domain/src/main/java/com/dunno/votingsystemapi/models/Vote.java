f59e3379b039a31eb08adf95495c0e08f3ab1055
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-25 17:52:22 -0500

    feat(vote-domain): add domain model Vote

---
+++ domain/src/main/java/com/dunno/votingsystemapi/models/Vote.java
@@ -0,0 +1,40 @@
+package com.dunno.votingsystemapi.models;
+
+import com.dunno.votingsystemapi.exceptions.InvalidFieldException;
+
+public class Vote {
+
+    private Long id;
+    private final Long voterId;
+    private final Long candidateId;
+
+    public Vote(Long voterId, Long candidateId) {
+        if (voterId == null) throw new InvalidFieldException("voterId", "Voter id cannot be null.");
+        if (candidateId == null) throw new InvalidFieldException("candidateId", "Candidate id cannot be null.");
+
+        this.voterId = voterId;
+        this.candidateId = candidateId;
+    }
+
+    private Vote(Long voterId, Long candidateId, Long id) {
+        this.voterId = voterId;
+        this.candidateId = candidateId;
+        this.id = id;
+    }
+
+    public static Vote restore(Long id, Long voterId, Long candidateId) {
+        return new Vote(id, voterId, candidateId);
+    }
+
+    public Long getCandidateId() {
+        return candidateId;
+    }
+
+    public Long getVoterId() {
+        return voterId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
