ba14ad4804f602ecce39b4fdc48e773e5510de08
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-26 07:36:55 -0500

    refactor(domain): convert domain objects for statistics into records

--- domain/src/main/java/com/dunno/votingsystemapi/models/CandidateResult.java
+++ domain/src/main/java/com/dunno/votingsystemapi/models/CandidateResult.java
@@ -1,34 +1,9 @@
 package com.dunno.votingsystemapi.models;
 
-public class CandidateResult {
-
-    private final Long candidateId;
-    private final String candidateName;
-    private final String party;
-    private final long votes;
-    private final double percentage;
-
-    public CandidateResult(Long candidateId, String candidateName, String party, long votes, double percentage) {
-        this.candidateId = candidateId;
-        this.candidateName = candidateName;
-        this.party = party;
-        this.votes = votes;
-        this.percentage = percentage;
-    }
-
-    public Long getCandidateId() {
-        return candidateId;
-    }
-    public String getCandidateName() {
-        return candidateName;
-    }
-    public String getParty() {
-        return party;
-    }
-    public long getVotes() {
-        return votes;
-    }
-    public double getPercentage() {
-        return percentage;
-    }
-}
+public record CandidateResult(
+        Long candidateId,
+        String candidateName,
+        String party,
+        long votes,
+        double percentage
+) {}
--- domain/src/main/java/com/dunno/votingsystemapi/models/VoteStatistics.java
+++ domain/src/main/java/com/dunno/votingsystemapi/models/VoteStatistics.java
@@ -2,27 +2,8 @@ package com.dunno.votingsystemapi.models;
 
 import java.util.List;
 
-public class VoteStatistics {
-
-    private final long totalVotesCast;
-    private final long totalVotersRegistered;
-    private final List<CandidateResult> results;
-
-    public VoteStatistics(long totalVotesCast, long totalVotersRegistered, List<CandidateResult> results) {
-        this.totalVotesCast = totalVotesCast;
-        this.totalVotersRegistered = totalVotersRegistered;
-        this.results = results;
-    }
-
-    public long getTotalVotesCast() {
-        return totalVotesCast;
-    }
-
-    public long getTotalVotersRegistered() {
-        return totalVotersRegistered;
-    }
-
-    public List<CandidateResult> getResults() {
-        return results;
-    }
-}
+public record VoteStatistics(
+        long totalVotesCast,
+        long totalVotersRegistered,
+        List<CandidateResult> results
+) {}
