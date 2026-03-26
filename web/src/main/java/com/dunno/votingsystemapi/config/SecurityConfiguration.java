1be4f81258d369727aabb02cc8357d150787639c
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-25 21:36:23 -0500

    feat(web): add security configuration class to handle incoming requests using the filter and matchers

---
+++ web/src/main/java/com/dunno/votingsystemapi/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.dunno.votingsystemapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfiguration(JwtFilter filter){
+        this.jwtFilter = filter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/auth/sign-in").anonymous()
+                        .requestMatchers(HttpMethod.POST, "/api/voters").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/candidates").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+}
