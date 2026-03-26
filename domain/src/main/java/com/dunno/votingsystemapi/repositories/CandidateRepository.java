869760c0d78de378d060c2fd0847955b131dceab
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-25 19:08:32 -0500

    feat(domain): add methods to repositories to find users by email. Necessary for sign in use case operation

--- domain/src/main/java/com/dunno/votingsystemapi/repositories/CandidateRepository.java
+++ domain/src/main/java/com/dunno/votingsystemapi/repositories/CandidateRepository.java
@@ -3,7 +3,10 @@ package com.dunno.votingsystemapi.repositories;
 import com.dunno.votingsystemapi.models.Candidate;
 import com.dunno.votingsystemapi.models.Email;
 
+import java.util.Optional;
+
 public interface CandidateRepository {
     Candidate save(Candidate candidate);
     boolean existsByEmail(Email email);
+    Optional<Candidate> findByEmail(Email email);
 }
--- domain/src/main/java/com/dunno/votingsystemapi/repositories/VoterRepository.java
+++ domain/src/main/java/com/dunno/votingsystemapi/repositories/VoterRepository.java
@@ -3,7 +3,10 @@ package com.dunno.votingsystemapi.repositories;
 import com.dunno.votingsystemapi.models.Email;
 import com.dunno.votingsystemapi.models.Voter;
 
+import java.util.Optional;
+
 public interface VoterRepository {
     Voter save(Voter voter);
     boolean existsByEmail(Email email);
+    Optional<Voter> findByEmail(Email email);
 }
