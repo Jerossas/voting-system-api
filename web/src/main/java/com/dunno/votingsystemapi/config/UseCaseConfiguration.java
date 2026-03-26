5e61694076e965b096d7d5929b7df2a3e424a24f
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-25 21:18:18 -0500

    feat(web): use cases bean configuration

---
+++ web/src/main/java/com/dunno/votingsystemapi/config/UseCaseConfiguration.java
@@ -0,0 +1,41 @@
+package com.dunno.votingsystemapi.config;
+
+import com.dunno.votingsystemapi.repositories.AdminRepository;
+import com.dunno.votingsystemapi.repositories.CandidateRepository;
+import com.dunno.votingsystemapi.repositories.VoterRepository;
+import com.dunno.votingsystemapi.services.PasswordEncoder;
+import com.dunno.votingsystemapi.usecases.auth.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfiguration {
+
+    @Bean
+    public RegisterVoterUseCase registerVoterUseCase(
+            VoterRepository voterRepository,
+            CandidateRepository candidateRepository,
+            PasswordEncoder  passwordEncoder
+    ){
+        return new RegisterVoterUseCaseImpl(voterRepository, candidateRepository, passwordEncoder);
+    }
+
+    @Bean
+    public RegisterCandidateUseCase registerCandidateUseCase(
+            VoterRepository voterRepository,
+            CandidateRepository candidateRepository,
+            PasswordEncoder  passwordEncoder
+    ){
+        return new RegisterCandidateUseCaseImpl(voterRepository, candidateRepository, passwordEncoder);
+    }
+
+    @Bean
+    public SignInUseCase signInUseCase(
+            VoterRepository voterRepository,
+            CandidateRepository candidateRepository,
+            PasswordEncoder  passwordEncoder,
+            AdminRepository adminRepository
+    ){
+        return new SignInUseCaseImpl(passwordEncoder, voterRepository, candidateRepository, adminRepository);
+    }
+}
